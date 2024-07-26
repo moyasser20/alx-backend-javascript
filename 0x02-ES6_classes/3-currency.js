d20410cf79f75da99747c59202a2e2e4bbe65d50
moyasser20 <mohamedyasser192023@gmail.com>
2024-07-26 09:23:32 -0700

    task3 done

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,29 +1,32 @@
-import { stringify } from "jest-matcher-utils";
-
 export default class Currency {
-    constructor(code, name) {
-        this.code = code;
-        this.name = name;
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if ((typeof code !== 'string') && !(code instanceof String)) {
+      throw new TypeError('Code must be a string');
     }
-    get code() {
-        return this._code;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if ((typeof name !== 'string') && !(name instanceof String)) {
+      throw new TypeError('Name must be a string');
     }
-    set code(code) {
-        if ((typeof code !== 'string') && !(code instanceof string)) {
-            throw new TypeError('Code must be a string');
-        }
-        this._code = code;
-    }
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        if ((typeof name !== 'string') && !(name instanceof string)) {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = name;
-    }
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
-    }
+    this._name = name;
+  }
 }
